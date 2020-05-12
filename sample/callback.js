ccdad991888ba42505d0f3bfedc7e3084077b8f2
Kim Minje <alswp0210@naver.com>
2020-05-12 14:15:26 +0900

    200512 Third Commit, add npm, sample folder

---
+++ sample/callback.js
@@ -0,0 +1,21 @@
+var fs = require('fs');
+
+function callReadFile(callback) {
+    fs.readFile('example/test.txt', 'utf8', function (err, result) {
+        if(err) {
+            console.error(err);
+            throw err;
+        }
+        else {
+            callback(result);
+        }
+    });
+}
+
+console.log('first func');
+console.error('second func');
+
+callReadFile(function (data) {
+    console.log(data);
+    console.log('third func');
+})
