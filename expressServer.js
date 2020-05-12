941677ae988d508bfa1360544f302acf29032593
Kim Minje <alswp0210@naver.com>
2020-05-12 16:58:54 +0900

    200512, use ejs, static assert

--- expressServer.js
+++ expressServer.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
- 
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views')); // ejs file location
+app.set('view engine', 'ejs'); // select view templet engine
+
+app.use(express.static(path.join(__dirname, 'public'))); // to use static asset
+
+
+
 app.get('/', function (req, res) {
-  res.send('Hello World')
+    var title = "javascript"
+  res.send('<html><h1>' + title + '</h1><h2>contents</h2></html>')
 })
- 
+
+app.get('/ejs', function (req, res) {
+    res.render('test')
+})
+
+app.get('/test', function (req, res) {
+    res.send('test page')
+})
+
+app.get('/design', function(req, res) {
+    res.render('designTest')    
+}) 
 app.listen(3000)
